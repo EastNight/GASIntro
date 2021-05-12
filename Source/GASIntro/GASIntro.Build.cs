38ce2afe57f2df7c5f3c72a190090cddb09ab971
kitappchen <kitappchen@tencent.com>
2021-05-12 17:38:56 +0800

    【动画，移动基础搞定，课程任务13结束状态】

--- Source/GASIntro/GASIntro.Build.cs
+++ Source/GASIntro/GASIntro.Build.cs
@@ -8,7 +8,7 @@ public class GASIntro : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore","GameplayAbilities","GameplayTags","GameplayTasks" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
